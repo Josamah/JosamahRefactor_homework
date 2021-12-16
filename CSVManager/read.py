8507f58dc76ac3ffab88bb3933453e7a1d73b371
Joshua samah <js2593@njit.edu>
2021-12-15 23:17:51 -0500

    project3

---
+++ CSVManager/read.py
@@ -0,0 +1,21 @@
+import pandas as pd
+data= pd.read_csv("Addition.csv")
+data.columns
+data.Addition
+
+import pandas as pd
+data= pd.read_csv("subtraction.csv")
+data.columns
+data.Subtraction
+
+import pandas as pd
+data= pd.read_csv("multiplication.csv")
+data.columns
+data.multiplication
+
+import pandas as pd
+data= pd.read_csv("division.csv")
+data.columns
+data.division
+
+
---
+++ CSVManager/write.py
@@ -0,0 +1,19 @@
+import pandas as pd
+data= pd.write_csv("Addition.csv")
+data.columns
+data.Addition
+
+import pandas as pd
+data= pd.write_csv("subtraction.csv")
+data.columns
+data.Subtraction
+
+import pandas as pd
+data= pd.write_csv("multiplication.csv")
+data.columns
+data.multiplication
+
+import pandas as pd
+data= pd.write_csv("division.csv")
+data.columns
+data.division
